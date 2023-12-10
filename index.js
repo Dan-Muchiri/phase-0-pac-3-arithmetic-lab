940428b54d8e111c22578005e2ab0de421499332
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-10 17:09:48 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+    let c = a + b;
+    return(c);
+}
+function subtract(a,b){
+    let c = a - b;
+    return(c);
+}
+function multiply(a,b){
+    let c = a * b;
+    return(c);
+}
+function divide(a,b){
+    let c = a / b;
+    return(c);
+}
+function increment(n){
+    let c = ++n;
+    return(c);
+}
+function decrement(n){
+    n--;
+    let c = n;
+    return(c);
+}
+function makeInt(n){
+    let c = parseInt(n,10);
+    return(c);
+}
+function preserveDecimal(n){
+    let c = parseFloat(n);
+    return(c);
+}
